2b0be88d624657389bf7e6afccb2573578a87353
20200731-a-test <tanaka.hideki@example.com>
2020-08-03 13:31:43 +0900

    update

--- iframe.js
+++ iframe.js
@@ -1,3 +1,3 @@
 document.open();
-document.write('<iframe src="./iframe.html"></iframe>');
+document.write('<iframe src="https://20200731-b-test.github.io/test-b.github.io/iframe.html"></iframe>');
 document.close();
